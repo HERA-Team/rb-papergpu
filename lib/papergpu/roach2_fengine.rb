d2d8eff8776be77540d98cb9a00fc6903d66f891
David MacMahon <davidm@astro.berkeley.edu>
2013-08-21 12:22:40 -0700

    Add high level functionality to roach2_fengine.rb

--- lib/papergpu/roach2_fengine.rb
+++ lib/papergpu/roach2_fengine.rb
@@ -82,16 +82,16 @@ module Paper
       :fid               => :rwreg,
       :input_rms_enable  => :rwreg,
       :input_rms_levels  => :bram,
-      :input_sel0        => :rwreg,
-      :input_sel1        => :rwreg,
-      :input_sel2        => :rwreg,
-      :input_sel3        => :rwreg,
-      :monitor_sel       => :rwreg,
+      :input_sel0        => [:rwreg, :insel_0],
+      :input_sel1        => [:rwreg, :insel_1],
+      :input_sel2        => [:rwreg, :insel_2],
+      :input_sel3        => [:rwreg, :insel_3],
+      :monitor_sel       => [:rwreg, :monsel],
       :seed_0            => :rwreg,
       :seed_1            => :rwreg,
       :seed_2            => :rwreg,
       :seed_3            => :rwreg,
-      :sync_arm          => :rwreg,
+      :sync_arm          => :skip, # Use specific methods
       :sync_count        => :roreg,
       :sync_period       => :roreg,
       :sync_uptime       => [:roreg, :uptime],
@@ -117,6 +117,36 @@ module Paper
       sleep secs
       toc = sys_clkcounter
       (toc - tic) % (1<<32) / scale / secs
+    end
+
+    # Arms the internal sync generator.  The sync will occur on the rising edge
+    # of the next external (or internal) sync pulse.
+    def arm_sync
+      val = wordread(:sync_arm)
+      val &= ~1 # Turn off desired bit
+      wordwrite(:sync_arm, val)
+      wordwrite(:sync_arm, val | 1)
+      wordwrite(:sync_arm, val)
+    end
+
+    # Arms all internal noise generators.  The noise generators will re-seed on
+    # the rising edge of the next external (or internal) sync pulse.  The
+    # current design does not allow arming individual noise generators.
+    def arm_noise
+      val = wordread(:sync_arm)
+      val &= ~2 # Turn off desired bit
+      wordwrite(:sync_arm, val)
+      wordwrite(:sync_arm, val | 2)
+      wordwrite(:sync_arm, val)
+    end
+
+    # Generates a software sync (for use when there is no external sync source).
+    def gen_sw_sync
+      val = wordread(:sync_arm)
+      val &= ~0x10 # Turn off desired bit
+      wordwrite(:sync_arm, val)
+      wordwrite(:sync_arm, val | 0x10)
+      wordwrite(:sync_arm, val)
     end
 
     # Returns or sets input selectors.  If +opts+ is a Fixnum, Range, or Array,
@@ -152,11 +182,11 @@ module Paper
                    else next
                    end
           inputs.each do |i|
-            insel_idx   = i / 8;
-            insel_shift = (i % 8) * 4;
-            insel_mask = (0b11) << insel_shift;
+            insel_idx   = i / 8
+            insel_shift = (i % 8) * 4
+            insel_mask = (0b11) << insel_shift
             # Mask off current bits
-            insel_vals[insel_idx] &= ~insel_mask;
+            insel_vals[insel_idx] &= ~insel_mask
             # Or in new bits
             insel_vals[insel_idx] |= (selval << insel_shift)
           end # inputs.each
@@ -189,6 +219,134 @@ module Paper
         (regval >> shift) & DELAY_MASK
       end
     end
+
+    # Gets or sets EQ coeffs.  Valid input values are in the range 0..31.
+    # Valid channel values are in the range 0..1023.  Valid eq_val values, when
+    # setting eq coefficients, are in the range 0 to 2047+127/128 (the value is
+    # treated as a Fix18_7).  If eq_val is not given, this function returns the
+    # reqested eq values.  If eq_val is given, it should be a single value.
+    #
+    # If input responds to #each (or #map), then the eq values will be set (or
+    # returned) for each input.
+    #
+    # If chan responds to #each (or #map), then the eq values will be set (or
+    # returned) for each channel.
+    #
+    # This method is not very efficient for getting or setting multiple
+    # channels for a given input.  Setting or getting multiple channels is more
+    # efficient via direct BRAM manipulation.
+    def eq(input, chan, eq_val=nil)
+      if eq_val # SET
+        # If input responds to each
+        if input.respond_to? :each
+          input.each {|i| eq(i, chan, eq_val)}
+        # Else, if chan responds to each
+        elsif chan.respond_to? :each
+          chan.each {|c| eq(input, c, eq_val)}
+        else
+          # Convert eq_val to integer
+          eq_val = (eq_val * 128).round
+
+          # If input is in the upper half of inputs, put eq_val in the upper half
+          # of the bram for the corresponding input from the lower half.
+          if input >= 16
+            input -= 16
+            chan += 1024
+          end
+
+          # Get bram object for input and store eq_val at desired channel
+          bram = send("eq_#{input}_coeffs")
+          bram[chan] = eq_val
+        end
+        # Return self when setting
+        self
+      else # GET
+        # If input responds to map
+        if input.respond_to? :map
+          input.map {|i| eq(i, chan, eq_val)}
+        # Else, if chan responds to map
+        elsif chan.respond_to? :each
+          chan.map {|c| eq(input, c, eq_val)}
+        else
+          # If input is in the upper half of inputs, put eq_val in the upper half
+          # of the bram for the corresponding input from the lower half.
+          if input >= 16
+            input -= 16
+            chan += 1024
+          end
+
+          # Get bram object for input and then get eq_val at desired channel
+          bram = send("eq_#{input}_coeffs")
+          eq_val = bram[chan] & ((1<<18)-1)
+
+          # Return as Rational (TODO: return as Float or Fixnum instead?)
+          Rational(eq_val, 128)
+        end
+      end
+    end
+
+    # Get or set the value (0 or 1) of an eth_ctrl bit
+    def eth_ctrl_bit(b, v=nil)
+      if v.nil?
+        # Get
+        (eth_ctrl >> b) & 1
+      else
+        # Set
+        reg_val = eth_ctrl
+        reg_val &= ~(1 << b)
+        reg_val |= (v&1) << b
+        self.eth_ctrl = reg_val
+      end
+    end
+    protected :eth_ctrl_bit
+
+    # Get the value (0 or 1) of the ethernet counter reset bit.
+    #
+    # The names of the ethernet counters are:
+    #
+    #   eth_N_sw_bframes
+    #   eth_N_sw_oflows
+    #   eth_N_sw_oruns
+    #   eth_N_gpu_oflows
+    #
+    # where N is 0, 1, 2, or 3.
+    def eth_cnt_rst; eth_ctrl_bit(8); end
+
+    # Set the value (0 or 1) of the ethernet counter reset bit.
+    #
+    # The names of the ethernet counters are:
+    #
+    #   eth_N_sw_bframes
+    #   eth_N_sw_oflows
+    #   eth_N_sw_oruns
+    #   eth_N_gpu_oflows
+    #
+    # where N is 0, 1, 2, or 3.
+    def eth_cnt_rst=(val); eth_ctrl_bit(8, val); end
+
+    # Get the value (0 or 1) of the "gpu" ethernet reset bit.
+    def eth_gpu_rst; eth_ctrl_bit(5); end
+
+    # Set the value (0 or 1) of the "gpu" ethernet reset bit.
+    def eth_gpu_rst=(val); eth_ctrl_bit(5, val); end
+
+    # Get the value (0 or 1) of the "switch" ethernet reset bit.
+    def eth_sw_rst; eth_ctrl_bit(4); end
+
+    # Set the value (0 or 1) of the "switch" ethernet reset bit.
+    def eth_sw_rst=(val); eth_ctrl_bit(4, val); end
+
+    # Get the value (0 or 1) of the "gpu" ethernet enable bit.
+    def eth_gpu_en; eth_ctrl_bit(1); end
+
+    # Set the value (0 or 1) of the "gpu" ethernet enable bit.
+    def eth_gpu_en=(val); eth_ctrl_bit(1, val); end
+
+    # Get the value (0 or 1) of the "switch" ethernet enable bit.
+    def eth_sw_en; eth_ctrl_bit(0); end
+
+    # Set the value (0 or 1) of the "switch" ethernet enable bit.
+    def eth_sw_en=(val); eth_ctrl_bit(0, val); end
 
   end # class Roach2Fengine
 end # module Paper
