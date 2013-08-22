c868b42a9449a0496956dffd8e811d9474b0f10c
David MacMahon <davidm@astro.berkeley.edu>
2013-08-22 16:39:08 -0700

    Add some PAPER switch command files
    These command files configure the paper switches for use with the PSA256
    correlator.  Both switches, ctlo and cthi, are intended to be setup
    identically, so these command files should be run on each switch (using
    bin/paper_switch_config.rb).

---
+++ stp_disable.cmd
@@ -0,0 +1,78 @@
+enable
+config
+no isdp enable
+interface te1/0/1
+spanning-tree disable
+interface te1/0/2
+spanning-tree disable
+no lldp transmit
+interface te1/0/3
+spanning-tree disable
+no lldp transmit
+interface te1/0/4
+spanning-tree disable
+no lldp transmit
+interface te1/0/5
+spanning-tree disable
+no lldp transmit
+interface te1/0/6
+spanning-tree disable
+no lldp transmit
+interface te1/0/7
+spanning-tree disable
+no lldp transmit
+interface te1/0/8
+spanning-tree disable
+no lldp transmit
+interface te1/0/9
+spanning-tree disable
+no lldp transmit
+interface te1/0/10
+spanning-tree disable
+no lldp transmit
+interface te1/0/11
+spanning-tree disable
+no lldp transmit
+interface te1/0/12
+spanning-tree disable
+no lldp transmit
+interface te1/0/13
+spanning-tree disable
+no lldp transmit
+interface te1/0/14
+spanning-tree disable
+no lldp transmit
+interface te1/0/15
+spanning-tree disable
+no lldp transmit
+interface te1/0/16
+spanning-tree disable
+no lldp transmit
+interface te1/0/17
+spanning-tree disable
+no lldp transmit
+interface te1/0/18
+spanning-tree disable
+no lldp transmit
+interface te1/0/19
+spanning-tree disable
+no lldp transmit
+interface te1/0/20
+spanning-tree disable
+no lldp transmit
+interface te1/0/21
+spanning-tree disable
+no lldp transmit
+interface te1/0/22
+spanning-tree disable
+no lldp transmit
+interface te1/0/23
+spanning-tree disable
+no lldp transmit
+interface te1/0/24
+spanning-tree disable
+no lldp transmit
+exit
+exit
+exit
+exit
