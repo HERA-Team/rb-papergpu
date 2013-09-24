83a27c6dd626e6d6da265d9f2f68bf9c6b74e2fe
David MacMahon <davidm@astro.berkeley.edu>
2013-09-24 14:54:00 -0700

    Change OPTS.adc_rms to OPTS.in_rms

--- paper_signal_levels.rb
+++ paper_signal_levels.rb
@@ -7,7 +7,7 @@ require 'ostruct'
 require './quantgain'
 
 OPTS = OpenStruct.new
-OPTS.adc_rms = 16
+OPTS.in_rms = 16
 OPTS.nshift = 12
 OPTS.eq = 1500
 
@@ -19,8 +19,8 @@ OptionParser.new do |op|
   op.separator('Compute PAPER F engine output levels.')
   op.separator('')
   #op.on('-q', '--quant', dest='quantize', default=4, type='int',help='How many bits to quantize to. ')
-  op.on('-i', '--in-rms=RMS', Float, "RMS value of input signal [#{OPTS.adc_rms}]") do |o|
-    OPTS.adc_rms = o
+  op.on('-i', '--in-rms=RMS', Float, "RMS value of input signal [#{OPTS.in_rms}]") do |o|
+    OPTS.in_rms = o
   end
   op.on('-f', '--fft=NSHIFT', Integer, "Number of shifts in the fft [#{OPTS.nshift}]") do |o|
     OPTS.nshift = o
@@ -36,9 +36,9 @@ end.parse!
 
 NSTAGES = 12
 
-pf_rms, fft_rms, reim_rms, eq_rms, quant_rms = paper_levels(OPTS.adc_rms, NSTAGES, (1<<OPTS.nshift)-1, OPTS.eq)
+pf_rms, fft_rms, reim_rms, eq_rms, quant_rms = paper_levels(OPTS.in_rms, NSTAGES, (1<<OPTS.nshift)-1, OPTS.eq)
 
-printf "ADC   output RMS %f\n", OPTS.adc_rms
+printf "ADC   output RMS %8.4f counts\n", OPTS.in_rms
 printf "PFB   output RMS %f\n", pf_rms
 printf "FFT   output RMS %f\n", fft_rms
 printf "ReIm  output RMS %f\n", reim_rms
